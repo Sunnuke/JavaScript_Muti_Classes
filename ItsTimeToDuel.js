baa4c88baee5c376a63bac24323d0b20d155280c
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-04 22:46:32 -0800

    Updated play method in Effect class to modify the correct stat of it's target

--- ItsTimeToDuel.js
+++ ItsTimeToDuel.js
@@ -36,7 +36,12 @@ class Effect extends Card {
     // METHODS
     play( target ) {
         if( target instanceof Unit ) {
-            target.res += this.magnitude
+            if (this.stat == "resilience") {
+                target.res += this.magnitude
+            }
+            else if (this.stat == "power") {
+                target.power += this.magnitude
+            } 
         } else {
             throw new Error( "Target must be a unit!" );
         }
@@ -50,7 +55,7 @@ console.log(redUnitCard);
 const algoEffect = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", 'resilience', +3);
 console.log(algoEffect);
 algoEffect.play(redUnitCard);
-console.log(redUnitCard.name + " resilience after effect: " + redUnitCard.res);
+console.log(redUnitCard.name + " " + algoEffect.stat + " after effect: " + redUnitCard.res);
 
 // Turn 2:
 const blackUnitCard = new Unit('Black Belt Ninja', 4, 5, 4);
@@ -58,7 +63,7 @@ console.log(blackUnitCard);
 const rejectionEffect = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience", -2);
 console.log(rejectionEffect);
 rejectionEffect.play(redUnitCard);
-console.log(redUnitCard.name + " resilience after effect: " + redUnitCard.res);
+console.log(redUnitCard.name + " " + rejectionEffect.stat + " after effect: " + redUnitCard.res);
 
 // Turn 3:
 const pairEffect = new Effect("Pair Programming", 3, "increase target's power by 2", "power", +2);
