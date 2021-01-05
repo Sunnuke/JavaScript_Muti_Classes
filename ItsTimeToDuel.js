e0cfb49f30b259b4683b6740379d357dff10f9da
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-04 21:48:05 -0800

    Created basic layout for the children classes, named Unit and Effect, with some attributes

--- ItsTimeToDuel.js
+++ ItsTimeToDuel.js
@@ -1,6 +1,30 @@
+// PARENT CLASS
 class Card {
+    // ATTRIBUTES
     constructor(name, cost) {
         this.name = name;
         this.cost = cost;
+    }
+}
+
+// CHILD CLASS
+class Unit extends Card {
+    // ATTRIBUTES
+    constructor(name, cost, power, res) {
+        super(name, cost) {
+            this.power = power;
+            this.res = res;
+        }
+    }
+}
+
+class Effect extends Card {
+    // ATTRIBUTES
+    constructor(name, cost, text, stat, magnitude) {
+        super(name, cost) {
+            this.text = text;
+            this.stat =stat;
+            this.magnitude = magnitude;
+        }
     }
 }
